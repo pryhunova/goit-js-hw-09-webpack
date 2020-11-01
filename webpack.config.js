c8a697066e8ee8eb1460cff799fc8c0abda1081f
Nata Pryhunova <prygunova@gmail.com>
2020-11-01 19:23:27 +0200

    webpack с настройками

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+   module: {
+     rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+       },
+       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],          
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  }
